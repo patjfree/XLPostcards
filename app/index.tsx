22886ba17e99b96d91abb4164184501b7b16b0dc
patjfree <patrick.freeburger@gmail.com>
2025-05-02 13:37:28 -0700

    chat gpt 3.5turbo does not support images --> going back to 4.0

--- app/index.tsx
+++ app/index.tsx
@@ -330,7 +330,7 @@ export default function HomeScreen() {
           'Authorization': `Bearer ${apiKey}`
         },
         body: JSON.stringify({
-          model: "gpt-3.5-turbo",
+          model: "gpt-4o",
           messages: [
             {
               role: "user",
