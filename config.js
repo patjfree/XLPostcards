d96af51e693a4a9342563834bd8155d714f045eb
patjfree <patrick.freeburger@gmail.com>
2025-05-12 21:37:10 -0700

    fixed the stripe live vs test key and updated favicons

--- config.js
+++ config.js
@@ -1,17 +1,24 @@
 import {
   OPENAI_API_KEY as ENV_OPENAI_API_KEY,
   STANNP_API_KEY as ENV_STANNP_API_KEY,
-  STRIPE_PUBLISHABLE_KEY as ENV_STRIPE_PUBLISHABLE_KEY
+  STRIPE_PUBLISHABLE_KEY_TEST as ENV_STRIPE_PUBLISHABLE_KEY_TEST,
+  STRIPE_PUBLISHABLE_KEY_LIVE as ENV_STRIPE_PUBLISHABLE_KEY_LIVE
 } from '@env';
 import Constants from 'expo-constants';
 import 'react-native-get-random-values';
 
+const APP_VARIANT = Constants.expoConfig?.extra?.APP_VARIANT || 'production';
+
 const config = {
-  APP_VARIANT: Constants.expoConfig?.extra?.APP_VARIANT || 'production',
+  APP_VARIANT,
   NODE_ENV: Constants.expoConfig?.extra?.NODE_ENV || 'production',
   OPENAI_API_KEY: ENV_OPENAI_API_KEY || Constants.expoConfig?.extra?.openAiApiKey || '',
   STANNP_API_KEY: ENV_STANNP_API_KEY || Constants.expoConfig?.extra?.stannpApiKey || '',
-  STRIPE_PUBLISHABLE_KEY: ENV_STRIPE_PUBLISHABLE_KEY || Constants.expoConfig?.extra?.stripePublishableKey || '',
+  STRIPE_PUBLISHABLE_KEY:
+    (APP_VARIANT === 'development'
+      ? ENV_STRIPE_PUBLISHABLE_KEY_TEST
+      : ENV_STRIPE_PUBLISHABLE_KEY_LIVE) ||
+    Constants.expoConfig?.extra?.stripePublishableKey || '',
 };
 
 // Make sure these are available globally
